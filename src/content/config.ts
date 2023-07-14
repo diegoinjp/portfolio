d02e0ecce2ea41f61e3168ffe5bdbb08d87d5bf0
Diego Vallejo <vallejo.diego@gridpredict.co.jp>
2023-07-14 16:36:33 +0900

    Initial commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,48 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
-});
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+})
 
-export const collections = { blog };
+const actor = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    age: z.number(),
+    hobbies: z.array(z.string()),
+    height: z.number().optional(),
+    mbti: z.string().optional(),
+    skills: z.array(z.string()).optional(),
+  }),
+})
+
+const project = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    imageUrl1: z.string().optional(),
+    imageUrl2: z.string().optional(),
+    imageUrl3: z.string().optional(),
+  }),
+})
+
+export const collections = { blog, actor, project }
